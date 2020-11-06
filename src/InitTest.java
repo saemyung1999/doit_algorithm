f23f45c48a70e81267ca591e4cb5ba82476c3ca8
saemyung1999 <saemyung1999@gmail.com>
2020-11-06 21:39:39 +0900

    Initial commit

---
+++ src/InitTest.java
@@ -0,0 +1,15 @@
+public class InitTest {
+
+    private static String a;
+    private static int b;
+
+    public static void main(String[] args) {
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+    }
+
+//    public static int test() {
+//        int c;
+//        return c;
+//    }
+}
