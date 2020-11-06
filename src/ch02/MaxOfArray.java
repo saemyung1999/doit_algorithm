0b2c0ff568d2afa3974c9f0f852c6ae78ee2b950
saemyung1999 <saemyung1999@gmail.com>
2020-11-06 22:11:40 +0900

    알고리즘 - 배열 최댓값 구하기

---
+++ src/ch02/MaxOfArray.java
@@ -0,0 +1,32 @@
+package ch02;
+
+import java.util.Scanner;
+
+public class MaxOfArray {
+
+    static int maxOf(int[] a) {
+        int max = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] > max) {
+                max = a[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("키의 최댓값 구하기");
+        System.out.print("사람 수: ");
+        int num = scanner.nextInt();
+
+        int[] height = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            System.out.print("height[" + i + "]: ");
+            height[i] = scanner.nextInt();
+        }
+
+        System.out.println("최댓값은 " + maxOf(height) + "입니다.");
+    }
+}
