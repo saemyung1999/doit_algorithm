4c7edad9b5e6b70fb11308d772bb1c982c95d0d2
saemyung1999 <saemyung1999@gmail.com>
2020-11-14 23:56:58 +0900

    알고리즘 - 배열 비교하기

---
+++ src/ch02/ArrayCompare/Practice5.java
@@ -0,0 +1,31 @@
+package ch02.ArrayCompare;
+
+import java.util.Scanner;
+
+public class Practice5 {
+
+    static void copy(int[] a, int[] b) {
+        for (int i = 0; i < b.length; i++) {
+            a[b.length - i - 1] = b[i];
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("배열 b의 요솟수: ");
+        int nb = scanner.nextInt();
+
+        int[] a = new int[nb];
+        int[] b = new int[nb];
+
+        for (int i = 0; i < nb; i++) {
+            System.out.print("b[" + i + "]: ");
+            b[i] = scanner.nextInt();
+        }
+        copy(a, b);
+
+        for (int i = 0; i < b.length; i++) {
+            System.out.println("a[" + i + "]: " + a[i]);
+        }
+    }
+}
